58f267bacadd52184dc8b79c7464d01be03e7015
I-AM-INSANE <vasiliyqqerty@gmail.com>
2021-01-17 11:05:36 +0300

    FirstCommit_SpaceShip

---
+++ Assets/Scripts/Driver.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driver : MonoBehaviour
+{
+    // Start is called before the first frame update
+    const float MovePerSeconds = 20f;
+    float turnRad = Mathf.PI / 2;
+    Rigidbody2D rigidSpaceShip;
+    void Start()
+    {
+        rigidSpaceShip = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float direction = Input.GetAxis("Turn");
+        if (direction != 0)
+        {
+            turnRad += -direction * 0.02f;
+            transform.Rotate(0, 0, -direction * 1.15f);
+        }
+
+        if (Input.GetAxis("DriveShip") != 0)
+        {
+            Vector2 vector = new Vector2(Mathf.Cos(turnRad), Mathf.Sin(turnRad*1));//jo;j;;
+            vector *= MovePerSeconds;
+            rigidSpaceShip.AddForce(vector, ForceMode2D.Force);
+        }
+    }
+}
---
+++ Assets/Scripts/SpaceShip.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceShip : MonoBehaviour
+{
+    // Start is called before the first frame update
+    bool visible = true;
+    Vector3 position;
+    float selfWidth, selfHeight;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void OnBecameInvisible()
+    {
+        /*RectTransform rectTransform = GetComponent<RectTransform>();
+        selfHeight = rectTransform.rect.height;
+        selfWidth = rectTransform.rect.width;*/
+        position = gameObject.transform.position;
+        if (position.x > ScreenUtils.ScreenRight || position.x < ScreenUtils.ScreenLeft)
+            position.x = -position.x;
+        if (position.y > ScreenUtils.ScreenTop || position.y < ScreenUtils.ScreenBottom)
+            position.y = -position.y;
+        gameObject.transform.position = position;
+    }
+}
