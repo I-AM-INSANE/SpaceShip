61fc4406df66a9d1c9c470d1b6325eb80f4c8f43
I-AM-INSANE <vasiliyqqerty@gmail.com>
2021-01-20 18:18:19 +0300

    Update

--- Assets/Scripts/Asteroid.cs
+++ Assets/Scripts/Asteroid.cs
@@ -7,16 +7,33 @@ public class Asteroid : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        float angle = Random.Range(0, Mathf.PI * 2);
-        Vector2 direction = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
-        float magnitude = Random.Range(2, 3);
+        float screenWidth = ScreenUtils.ScreenRight - ScreenUtils.ScreenLeft;
+        float screenHeight = ScreenUtils.ScreenTop - ScreenUtils.ScreenBottom;
+
+        float X = Random.Range(Camera.main.transform.position.x - gameObject.transform.position.x - screenWidth / 2.3f, Camera.main.transform.position.x - gameObject.transform.position.x + screenWidth / 2.3f);
+        float Y = Random.Range(Camera.main.transform.position.y - gameObject.transform.position.y - screenHeight / 2.3f, Camera.main.transform.position.y - gameObject.transform.position.y + screenHeight / 2.3f);
+        Vector2 direction = new Vector2(X, Y);
+        direction.Normalize();
+        float magnitude = Random.Range(4, 5);
         Rigidbody2D asteroid = gameObject.GetComponent<Rigidbody2D>();
         asteroid.AddForce(direction * magnitude, ForceMode2D.Impulse);
+
+
+        GameObject[] asteroids = GameObject.FindGameObjectsWithTag("Asteroid");
+        foreach (GameObject obj in asteroids)
+        {
+            Physics2D.IgnoreCollision(obj.gameObject.GetComponent<Collider2D>(), gameObject.GetComponent<Collider2D>());
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        Destroy(collision.gameObject);
     }
 }
--- Assets/Scripts/AsteroidSpawner.cs
+++ Assets/Scripts/AsteroidSpawner.cs
@@ -10,32 +10,45 @@ public class AsteroidSpawner : MonoBehaviour
     [SerializeField]
     Sprite[] sprites = new Sprite[3];
 
-    Timer timer;
+    // Timer timer;
     List<GameObject> asteroids = new List<GameObject>();
+    Vector2 spawnPosition;
     // Start is called before the first frame update
     void Start()
     {
-        timer = gameObject.AddComponent<Timer>();
-        timer.Duration = 2;
-        timer.Run();
+        // timer = gameObject.AddComponent<Timer>();
+        AsteroidSpawn();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (timer.Finished && asteroids.Count < 5)
-        {
-            asteroids.Insert(0, prefabAsteroid);
-            Vector2 spawnPosition = new Vector2(Random.Range(ScreenUtils.ScreenLeft, ScreenUtils.ScreenRight), Random.Range(ScreenUtils.ScreenTop, ScreenUtils.ScreenBottom));
-            Instantiate(asteroids[0], spawnPosition, Quaternion.identity);
-            ChangeSprite();
-            timer.Run();
-        }
+        ScreenUtils.Initialize();
     }
 
-    void ChangeSprite()
+    void ChangeSprite(GameObject prefabAsteroid)
     {
-        SpriteRenderer spriteRenderer = asteroids[0].GetComponent<SpriteRenderer>();
+        SpriteRenderer spriteRenderer = prefabAsteroid.GetComponent<SpriteRenderer>();
         spriteRenderer.sprite = sprites[Random.Range(0, 3)];
+    }
+
+    void AsteroidSpawn()
+    {
+        SpriteRenderer spriteRenderer = prefabAsteroid.GetComponent<SpriteRenderer>();
+        float asteroidWidth = spriteRenderer.sprite.bounds.size.x;
+        float asteroidHeight = spriteRenderer.sprite.bounds.size.y;
+
+        spawnPosition = new Vector2(ScreenUtils.ScreenRight - (ScreenUtils.ScreenRight - ScreenUtils.ScreenLeft) / 2, ScreenUtils.ScreenTop + asteroidHeight);
+        Instantiate(prefabAsteroid, spawnPosition, Quaternion.identity);
+        ChangeSprite(prefabAsteroid);
+        spawnPosition = new Vector2(ScreenUtils.ScreenRight + asteroidWidth, ScreenUtils.ScreenTop - (ScreenUtils.ScreenTop - ScreenUtils.ScreenBottom) / 2);
+        Instantiate(prefabAsteroid, spawnPosition, Quaternion.identity);
+        ChangeSprite(prefabAsteroid);
+        spawnPosition = new Vector2(ScreenUtils.ScreenRight - (ScreenUtils.ScreenRight - ScreenUtils.ScreenLeft) / 2, ScreenUtils.ScreenBottom - asteroidHeight);
+        Instantiate(prefabAsteroid, spawnPosition, Quaternion.identity);
+        ChangeSprite(prefabAsteroid);
+        spawnPosition = new Vector2(ScreenUtils.ScreenLeft - asteroidWidth, ScreenUtils.ScreenTop - (ScreenUtils.ScreenTop - ScreenUtils.ScreenBottom) / 2);
+        Instantiate(prefabAsteroid, spawnPosition, Quaternion.identity);
+        ChangeSprite(prefabAsteroid);
     }
 }
